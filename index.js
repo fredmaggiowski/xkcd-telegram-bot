7a9f93f682b95bcaf85e631c7a791ca263ea3d39
Federico Maggi <federicomaggi@me.com>
2015-11-05 17:25:22 +0100

    command processing template and package.json fixes, also added dependencies

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
+/*
+ *	xkcd telegram bot
+ *	Federico Maggi
+ *	05/11/2015
+ *
+ *	Thanks to Randall Munroe for all the fun!
+ */
+
+var debug = require('debug')('debug');
+
+var TelegramBot = require('node-telegram-bot-api');
+
 
 var token  = "token",
-		tghook = "https://api.telegram.org/bot";
+	  tghook = "https://api.telegram.org/bot"+token;
+
+var port = 7654,
+		host = '0.0.0.0';
+
+var bot = new TelegramBot(token, {webHook: {port: port, host: host}});
+
+bot.on('message', function msgReceived(msg){
+
+	debug(msg);
+
+	var chatId = msg.chat.id;
+	var msgarr = msg.split(" ");
+
+	switch(msgarr[0]){
+		case "/random": 
+			cmddebug("Random comic required");
+			break;
+
+		case "/getxkcd":
+			cmddebug("Required comic #",msgarr[1]);
+			break;
+
+		case "/latest":
+			cmddebug("Required latest comic");
+			break;
+
+		case "/test":
+			cmddebug("Test command fired");
+
+			bot.sendMessage(chatId, "Fuck @AlexLanGame");
+			break;
+
+		default:
+			cmddebug("Default case");
+			break;
+	}
+
+});
