c4d7cc7c4d880bacf5d97716c63e86371f488798
Federico Maggi <federicomaggi@me.com>
2019-04-01 11:50:01 +0200

    updates and refactoring

--- bot.js
+++ bot.js
@@ -7,77 +7,41 @@
  */
 'use strict'
 
+let TelegramBot = require('node-telegram-bot-api')
+let request = require('request')
+
 // Load configuration
-var ctx = require('./config/config.json')
+let ctx = require('./config/config.json')
 
-// Require debug and logging modules
-var debug  = require('debug')('debug'),
-    reqdbg = require('debug')('request'),
-    cmddbg = require('debug')('cmd')
+let token  = ctx.token;
+let tghook = 'https://'+ctx.hook.address+':'+ctx.hook.port+'/'+token;
 
-var bunyan = require('bunyan'),
-    log    = bunyan.createLogger(
-      {
-        name: 'xkcdbot',
-        streams: [
-          {
-            level: 'info',
-            path: ctx.logpath.info
-          },
-          {
-            level: 'warn',
-            path: ctx.logpath.warn
-          },
-          {
-            level: 'error',
-            path: ctx.logpath.err
-          } 
-        ]
-      }
-    )
+let theLatest = 0;
+let bot = new TelegramBot(token, {
+  webHook: {port: ctx.hook.port, host: ctx.hook.address, cert:ctx.cert.crt, key:ctx.cert.key}
+});
+bot.setWebHook(tghook, ctx.cert.crt);
 
-// Require useful modules
-var request = require('request')
+bot.on('message', (msg) => {
+  console.log(msg);
 
-// Require node.js telegram Bot API
-var TelegramBot = require('node-telegram-bot-api')
-
-var token  = ctx.token,
-    tghook = 'https://'+ctx.hook.address+':'+ctx.hook.port+'/'+token
-
-debug(ctx)
-debug(tghook)
-
-var theLatest = 0
-
-var bot = new TelegramBot(token, {webHook: {port: ctx.hook.port, host: ctx.hook.address, cert:ctx.cert.crt, key:ctx.cert.key}})
-bot.setWebHook(tghook, ctx.cert.crt) 
-
-bot.on('message', function msgReceived(msg){
-  
-  log.info(msg)
-
-  debug('message:', msg)
-
-  var chatId = msg.chat.id
-  var msgarr = msg.text.split(' ')
-
-  handleCommand( msgarr[0], function ( err, comic ){
-
-    if( err )
-      return
+  let chatId = msg.chat.id;
+  let msgarr = msg.text.split(' ');
+  handleCommand(msgarr[0], (err, comic) => {
+    if (err) {
+      return;
+    }
 
     // Do something with the comic.. like sending it via bot.sendMessage
-    comic = JSON.parse(comic)
-    debug(comic)
+    comic = JSON.parse(comic);
+		if (comic.title === 'help') {
+      // It's not the comic, it's the help page :)
+      return bot.sendMessage(chatId, comic.help);
+    }
 
-		if (comic.title === 'help') // It's not the comic, it's the help page :)
-			return bot.sendMessage(chatId, comic.help)
-
-    return bot.sendMessage(chatId, comic.title+'\n'+comic.img+'\n'+comic.alt)
-
-  }, msgarr) 
-})
+    return bot.sendMessage(chatId, comic.title+'\n'+comic.img+'\n'+comic.alt);
+  }, msgarr);
+});
 
 /*
  *  Handle given command
@@ -85,45 +49,38 @@ bot.on('message', function msgReceived(msg){
  *  @param {function} cb: callback function (err, reply)
  *  @param {String Array} pars: Optional extra paramethers
  */
-function handleCommand( cmd, cb, pars ){
-  var comic = {}
-  cmddbg(cmd)
-
-  switch(cmd.split('@')[0]){
+function handleCommand(cmd, cb, pars) {
+  switch (cmd.split('@')[0]) {
     case '/random': 
-      cmddbg('Random comic required')
-      var number = Math.floor(Math.random() * (theLatest + 1))
-      cmddbg('random number picked: %d', number)
-      return retrieveComic(number, cb)
+      let number = Math.floor(Math.random() * (theLatest + 1));
+      return retrieveComic(number, cb);
 
     case '/getxkcd':
-      if( pars === undefined )
-        return cb(JSON.stringify({error: 'No comic number provided'}), null)
+      if (pars === undefined) {
+        return cb(JSON.stringify({error: 'No comic number provided'}), null);
+      }
 
-      cmddbg('Required comic #',pars[0])
-      return retrieveComic(pars[1], cb)
+      return retrieveComic(pars[1], cb);
 
     case '/latest':
-      cmddbg('Required latest comic')
-      return retrieveComic(0, cb)
+      return retrieveComic(0, cb);
 
     case '/test':
-      cmddbg('Test command fired')
-      bot.sendMessage(chatId, 'Fuck @AlexLanGame')
+      bot.sendMessage(chatId, 'F*ck @AlexLanGame');
       break
 
     case '/now':
-      cmddbg('Now command fired')
-      return retrieveComic(1335, cb) // http://xkcd.com/now : 1335
+      return retrieveComic(1335, cb); // http://xkcd.com/now : 1335
 
     case '/help':
-      cmddbg('Help command')
-      return cb(null, JSON.stringify({title: 'help', help:'Help:\n\t- /getxkcd NUMBER -> get chosen comic\n\t- /random -> get a random comic\n\t- /latest -> get latest comic\n\t- /now'}))
+      return cb(null, JSON.stringify({
+        title: 'help',
+        help:'Help:\n\t- /getxkcd NUMBER -> get chosen comic\n\t- /random -> get a random comic\n\t- /latest -> get latest comic\n\t- /now',
+      }));
 
     default:
-      cmddbg('Default case')
-      log.error('Command not found %s',cmd)
-      return cb(JSON.stringify({error: 'command not found'}), null)
+      console.error('Command not found %s',cmd);
+      return cb(JSON.stringify({error: 'command not found'}), null);
   }
 }
 
@@ -132,40 +89,35 @@ function handleCommand( cmd, cb, pars ){
  *  @param {Number} number: Comic number (if 0 it takes the latest)
  *  @param {Function} cb: callback function 
  */
-function retrieveComic( number, cb ){
+function retrieveComic(number, cb) {
+  if (number > theLatest) {
+    cb(JSON.stringify({error: 'this comic does not exists, yet!'}),null);
+  }
   
-  if( number > theLatest )
-    cb(JSON.stringify({error: 'this comic does not exists, yet!'}),null)
-  
-  number = number === 0 ? '' : number
-  
-  var url = 'http://xkcd.com/'+number+'/info.0.json'
-  reqdbg('url: %s',url)
-  
+  number = number === 0 ? '' : number;  
+  let url = 'http://xkcd.com/'+number+'/info.0.json'; 
   request(url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      console.log(body)
-      cb(null, body)
+      console.log(body);
+      cb(null, body);
     }
-  })
+  });
 }
 
 /*
  *  Get latest comic with configured polling rate
  */
-setTimeout( function getLatestComicPolling( ){
-  
-  request('http://xkcd.com/info.0.json', function (error, response, body){
-    if( !error && response.statusCode === 200 ){
-      
-      if( theLatest < JSON.parse(body).num ){
+setTimeout(() => {
+  request('http://xkcd.com/info.0.json', (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      if (theLatest < JSON.parse(body).num) {
         // Hey! We've got a new comic!
 
         // Let's do somenthig amazing!
-        reqdbg('New comic found')
+        // reqdbg('New comic found')
       }
 
       theLatest = JSON.parse(body).num
-    } 
-  })
+    }
+  });
 }, ctx.polling)
